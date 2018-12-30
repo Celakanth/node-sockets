1d86688f7a45f1f9871a8c180617ee42e298b196
Christian Hoare <christian@Christians-MacBook-Pro.local>
2018-12-30 22:31:31 +0000

    Added new files

---
+++ public/js/index.js
@@ -0,0 +1,49 @@
+var socket = io();
+
+socket.on('connect', function(){
+  console.log('connect to server');
+});
+
+socket.on('disconnect', function(){
+  console.log('Disconnected from server');
+});
+
+socket.on('newChatMessage', function(message){
+  console.log('New chat message has arrived', message);
+});
+
+function sendMessage(){
+  var SendToId = $('#chatterId').val()
+  var TheMessage = $('#message').val();
+  if (SendToId != '') {
+    socket.emit('createMessage',{
+        messageText: TheMessage,
+        fromChaterId: SendToId,
+        createdAt: new Date().getDate()
+      }
+    );
+    $('#message').val('');
+      $('#serverMessage').html('');
+  }
+  else{
+    $('#serverMessage').html('No recipient selected');
+  };
+}
+
+function SetTalker(chaterId){
+  var oldClassId = $('#chatterId').val();
+  if (oldClassId != '') {
+    $('#' + oldClassId).removeClass('active');
+    $('#' + chaterId).addClass('active');
+  }
+  else{
+    $('#' + chaterId).addClass('active');
+  }
+  $('#chatterId').val(chaterId);
+};
+
+$(document).ready(function(){
+    $('#action_menu_btn').click(function(){
+    $('.action_menu').toggle();
+  });
+});
